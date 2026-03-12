bf4d46f7e5ae09e432b9f6c036b001f2c9b7136f
dksolo <dbm95mail@gmail.com>
2026-03-12 11:04:40 +0500

    fix: newline

--- classes/form/message_form.php
+++ classes/form/message_form.php
@@ -32,7 +32,7 @@ class message_form extends \moodleform {
      * Define the form.
      */
     public function definition() {
-        $mform = $this->_form; // Don't forget the underscore! 
+        $mform = $this->_form; // Don't forget the underscore!
 
         $mform->addElement('textarea', 'message', get_string('yourmessage', 'local_greetings'));
         $mform->setType('message', PARAM_TEXT);
