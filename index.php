bd155106f4d9a7ee6b8de8b8c3b9539621dd9578
dksolo <dbm95mail@gmail.com>
2026-03-12 08:43:43 +0500

    feat: page layout essentials

--- index.php
+++ index.php
@@ -21,3 +21,25 @@
  * @copyright   2026 Dmitrii Solopii <dksolo.web.dev@gmail.com>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+/*
+1. In the index.php file, add the following line after the copyright block.
+*/
+require_once('../../config.php'); 
+/* 
+Most php files that can be directly accessed begin with requiring the config.php file, located in the root of the Moodle folder.
+
+
+2. Next, set the page context. We will be using the system context for the purpose of this course.
+*/
+$context = context_system::instance();
+$PAGE->set_context($context);
+
+// 3. Set the page URL using the following code: 
+$PAGE->set_url(new moodle_url('/local/greetings/index.php'));
+
+// 4. We will use the standard page layout. The following code will set the page layout:
+
+$PAGE->set_pagelayout('standard');
+
+// 5. Save your work and access the index.php by typing the following url in your web browser: 
+// http://<your-moodle-site>/local/greetings/
